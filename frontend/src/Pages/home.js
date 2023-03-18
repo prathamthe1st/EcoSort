55e0b089b3d8db7c8af2b2c44346231c03fdc5d9
ameyayeole <yeoleameya8@gmail.com>
2023-03-18 17:04:15 +0530

    Frontend Initialized

---
+++ frontend/src/Pages/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Home() {
+        return (
+            <button><Link to="/signup">Go to My Route</Link></button>
+        );
+}
+
+export default Home;
+
