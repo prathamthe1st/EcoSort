bfffd36a80b9297763300db469a2b66178877379
Pratham Goenka <prathamg1803@gmail.com>
2023-03-18 18:36:35 +0530

    User Module completed

--- ecosort/user/urls.py
+++ ecosort/user/urls.py
@@ -1,6 +1,9 @@
-from .views import RegisterAPI
+from .views import *
+from knox import views as knox_views
 from django.urls import path
 
 urlpatterns = [
     path('register/', RegisterAPI.as_view(), name='register'),
+    path('login/', LoginAPI.as_view(), name='login'),
+    path('logout/', knox_views.LogoutView.as_view(), name='logout'),
 ]
